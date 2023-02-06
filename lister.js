e1d9ce29690eddd6dd6302458c47536edfdb0e4c
chinmay726632 <chinmaykumar432>
2023-02-06 16:35:42 +0530

    html

---
+++ lister.js
@@ -0,0 +1,5 @@
+ var header=document.getElementById('header-title');
+ header.style.borderBottom='solid 3px black';
+var add = document.querySelectorAll('.title');
+ add[0].style.fontWeight ='bold';
+ add[0].style.backgroundColor ='green';
