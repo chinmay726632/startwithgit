1263bdd95220614bf5a0b5dd9cce0259dd3c2ff4
chinmay726632 <chinmaykumar432>
2023-02-06 22:32:27 +0530

    add item change color

--- lister.js
+++ lister.js
@@ -4,5 +4,16 @@ var add = document.querySelectorAll('.title');
  add[0].style.fontWeight ='bold';
  add[0].style.backgroundColor ='green';
  var list = document.getElementsByClassName('list-group-items');
+ console.log(list);
+  for(var j=0;j<=list.length;j++)
+ {
+      list[j].style.backgroundColor='blue';
+ }
  list.style.fontWeight='bold';
  list[2].style.backgroundColor='green';
+ var li = document.getElementsByTagName('li');
+ for(var i=0;i<=li.length;i++)
+ {
+    li[i].style.backgroundColor='#f4f4f4';
+ }
+ 
