e30a1007653c051e107c9515dbaf9477eb187fc0
chinmay726632 <chinmaykumar432>
2023-02-06 21:30:31 +0530

    changing items

--- lister.js
+++ lister.js
@@ -3,3 +3,6 @@
 var add = document.querySelectorAll('.title');
  add[0].style.fontWeight ='bold';
  add[0].style.backgroundColor ='green';
+ var list = document.getElementsByClassName('list-group-items');
+ list.style.fontWeight='bold';
+ list[2].style.backgroundColor='green';
